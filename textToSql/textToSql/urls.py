0f4f2ed83a76bb10f38ccb1bba2abe34d78dcfd9
Asim Shah <asimshah2111@gmail.com>
2024-06-05 11:31:56 +0530

    added open ai and gemini

--- textToSql/textToSql/urls.py
+++ textToSql/textToSql/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import addDataSource, queryDataSource
+from .views import addDataSource, queryDataSource ,querydatasourceOpenAi ,querydatasourceGemini
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('addDataSource/', addDataSource, name='add_data_source'),
     path('queryDataSource/', queryDataSource, name='query_data_source'),
+    path('queryDataSourceOpenAI/',querydatasourceOpenAi,name='query_data_source_openAI'),
+    path('querydatasourceGemini/',querydatasourceGemini,name='query_data_source_Gemini')
 ]
--- textToSql/textToSql/views.py
+++ textToSql/textToSql/views.py
@@ -8,9 +8,13 @@ import chromadb
 import time
 from openai import RateLimitError
 import json
+from openai import OpenAI
+import google.generativeai as genai
+from pymongo import MongoClient
+mongo_client = MongoClient('mongodb://localhost:27017/')
+chat_db = mongo_client['chat_history']
+history_collection = chat_db['history']
 
-# Set your OpenAI API key
-openai.api_key = 'sk-proj-Qc091peQOuC9jZz0mJU4T3BlbkFJgyl01L5g03POMiaCBIGh'
 
 @csrf_exempt
 def addDataSource(request):
@@ -126,7 +130,6 @@ def queryDataSource(request):
                 
                 query:
                 {query_text}
-                
             """
             
             print(context)
@@ -142,6 +145,13 @@ def queryDataSource(request):
             print(response.text)
             
             if response.status_code == 200:
+                history_entry = {
+                    'user_query': query_text,
+                    'ai_response': response.text,
+                    'timestamp': time.time()
+                }
+                history_collection.insert_one(history_entry)
+                
                 return JsonResponse({'response': response.text})
             else:
                 return JsonResponse({'error': 'Unable to get response from the LLM.', 'details': response.text}, status=500)
@@ -150,3 +160,123 @@ def queryDataSource(request):
             return JsonResponse({'error': str(e)}, status=500)
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+
+@csrf_exempt
+def querydatasourceOpenAi(request):
+    if request.method == 'POST':
+        query_text = request.POST.get('query')
+
+        if not query_text:
+            return JsonResponse({'error': 'Query text is required'}, status=400)
+        
+        try:
+            chroma_client = chromadb.Client()
+            collection1 = chroma_client.get_collection(name="schema_embeddings_MYSQL")
+            
+            results1 = collection1.query(
+                query_texts=[query_text],
+                n_results=10
+            )
+            
+            collection2 = chroma_client.get_collection(name='relationship_embeddings_MYSQL')
+            
+            results2 = collection2.query(
+                query_texts=[query_text],
+                n_results=10
+            )
+
+            schema_context = str(results1['documents'])
+            relationship_context = str(results2['documents'])
+            
+            context = f"""
+                you are an expert MYSQL db developer. Below is the schema and 
+                relationship for the same. I will ask you to write more sql queries on top of that.
+                Make sure that you will not write any DDL or delete, update queries.
+                All the queries has to be of type select. Dont execute these queries 
+                just provide us the query . 
+                
+                Schema context:
+                {schema_context}
+                
+                relationship context:
+                {relationship_context}
+                
+                query:
+                {query_text}
+            """
+            print(context)
+            
+            client = OpenAI(api_key=API_KEY)
+
+            completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": context},
+                {"role": "user", "content": query_text}
+            ]
+            )
+            
+            return JsonResponse({'response': completion.choices[0].message})
+            
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+    
+    
+@csrf_exempt
+def querydatasourceGemini(request):
+    if request.method == 'POST':
+        query_text = request.POST.get('query')
+
+        
+        if not query_text:
+            return JsonResponse({'error': 'Query text is required'}, status=400)
+        
+        try:
+            chroma_client = chromadb.Client()
+            collection1 = chroma_client.get_collection(name="schema_embeddings_MYSQL")
+            
+            results1 = collection1.query(
+                query_texts=[query_text],
+                n_results=10
+            )
+            
+            collection2 = chroma_client.get_collection(name='relationship_embeddings_MYSQL')
+            
+            results2 = collection2.query(
+                query_texts=[query_text],
+                n_results=10
+            )
+
+            schema_context = str(results1['documents'])
+            relationship_context = str(results2['documents'])
+            
+            context = f"""
+                you are an expert MYSQL db developer. Below is the schema and 
+                relationship for the same. I will ask you to write more sql queries on top of that.
+                Make sure that you will not write any DDL or delete, update queries.
+                All the queries has to be of type select. Dont execute these queries 
+                just provide us the query . 
+                
+                Schema context:
+                {schema_context}
+                
+                relationship context:
+                {relationship_context}
+                
+                query:
+                {query_text}
+            """
+        
+            print(context)
+            genai.configure(api_key=GEMINI_API)
+            model = genai.GenerativeModel(model_name='gemini-1.5-flash-001',system_instruction=[context])    
+            response = model.generate_content(query_text)
+            return JsonResponse({'response': response.text})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+    
